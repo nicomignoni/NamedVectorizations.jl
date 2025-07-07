07d3b01f019063ea88f619c3c571244f848cd899
Nicola Mignoni <nicola.mignoni@poliba.it>
2025-07-07 15:10:41 +0200

    Add GitHub action for documentation

--- docs/make.jl
+++ docs/make.jl
@@ -8,5 +8,12 @@ makedocs(
         "Home" => "index.md",
         "API" => "api.md"
     ],
+    format = Documenter.HTML(
+        edit_link="master"
+    ),
     repo=Remotes.GitHub("nicomignoni", "NamedVectorizations.jl"),
+)
+
+deploydocs(
+    repo = "github.com/nicomignoni/NamedVectorizations.jl.git",
 )
